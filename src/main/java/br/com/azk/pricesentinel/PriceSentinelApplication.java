f3a3a1afa464606fc777f5e82f27187a3150d808
xAzKaR <leandro.glc@gmail.com>
2026-07-23 21:14:52 -0300

    feat: Primeiro commit

---
+++ src/main/java/br/com/azk/pricesentinel/PriceSentinelApplication.java
@@ -0,0 +1,13 @@
+package br.com.azk.pricesentinel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriceSentinelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PriceSentinelApplication.class, args);
+	}
+
+}
